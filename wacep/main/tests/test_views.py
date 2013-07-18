3a6d9cc7b8c72328b94972b4dbe509fa5a57a396
Anders Pearson <anders@columbia.edu>
2013-07-18 14:47:42 -0400

    couple basic tests with a logged in user

--- wacep/main/tests/test_views.py
+++ wacep/main/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.conf import settings
 from django.contrib.auth.models import User
+from pagetree.models import Hierarchy
 import factory
 import random
 import string
@@ -15,7 +16,7 @@ class BasicAnonTest(TestCase):
         with self.settings(SHOW_SPLASH=True):
             response = self.c.get("/")
             self.assertEquals(response.status_code, 200)
-            
+
     def test_splash_disabled(self):
         with self.settings(SHOW_SPLASH=False):
             response = self.c.get("/")
@@ -36,6 +37,24 @@ class UserFactory(factory.DjangoModelFactory):
     username = factory.LazyAttribute(lambda t: random_string())
 
 
+class HierarchyFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Hierarchy
+    name = "main"
+    base_url = ""
+
+
+def make_test_hierarchy():
+    h = HierarchyFactory()
+    h.get_root().add_child_section_from_dict(
+        {
+            'label': 'Section 1',
+            'slug': 'section-1',
+            'pageblocks': [],
+            'children': [],
+        })
+    return h
+
+
 class LoggedInTest(TestCase):
     def setUp(self):
         self.c = Client()
@@ -48,3 +67,18 @@ class LoggedInTest(TestCase):
         with self.settings(SHOW_SPLASH=True):
             response = self.c.get("/")
             self.assertEquals(response.status_code, 200)
+
+    def test_page(self):
+        h = make_test_hierarchy()
+        response = self.c.get("/section-1/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_edit_page(self):
+        h = make_test_hierarchy()
+        response = self.c.get("/edit/section-1/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_instructor_page(self):
+        h = make_test_hierarchy()
+        response = self.c.get("/instructor/section-1/")
+        self.assertEquals(response.status_code, 200)
