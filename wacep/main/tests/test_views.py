c6abbdf110519b07a49019c8dd26c66b4d5aaa47
Anders Pearson <anders@columbia.edu>
2013-07-18 14:32:24 -0400

    test root with logged in user

--- wacep/main/tests/test_views.py
+++ wacep/main/tests/test_views.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.conf import settings
+from django.contrib.auth.models import User
+import factory
+import random
+import string
 
 
-class BasicTest(TestCase):
+class BasicAnonTest(TestCase):
     def setUp(self):
         self.c = Client()
 
@@ -21,3 +25,26 @@ class BasicTest(TestCase):
         response = self.c.get("/smoketest/")
         self.assertEquals(response.status_code, 200)
         assert "PASS" in response.content
+
+
+def random_string(length=10):
+    return u''.join(random.choice(string.ascii_letters) for x in range(length))
+
+
+class UserFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = User
+    username = factory.LazyAttribute(lambda t: random_string())
+
+
+class LoggedInTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.u = UserFactory()
+        self.u.set_password("test")
+        self.u.save()
+        self.c.login(username=self.u.username, password="test")
+
+    def test_splash_enabled(self):
+        with self.settings(SHOW_SPLASH=True):
+            response = self.c.get("/")
+            self.assertEquals(response.status_code, 200)
