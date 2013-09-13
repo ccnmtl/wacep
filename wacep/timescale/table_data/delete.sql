8b3c22051708b8728258cf485641547b5d0a4196
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2013-09-13 12:47:38 -0400

    fixing delete,sql

---
+++ wacep/timescale/table_data/delete.sql
@@ -0,0 +1,5 @@
+delete from timescale_inputcombination;
+delete from timescale_yearinput ;
+delete from timescale_graphingmodeinput;
+delete from timescale_seasoninput;
+delete from timescale_activitystate;
