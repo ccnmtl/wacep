b7cd3cb856e62f5c5b2bd57b4264bbd167a7086f
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2013-10-30 15:09:47 -0400

    update

--- wacep/certificates/tests/test_models.py
+++ wacep/certificates/tests/test_models.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
-
+from wacep.timescale.models import YearInput, GraphingModeInput, SeasonInput, ActivityState, TimescaleBlock
+from datetime import datetime
 
 class BasicModelTest(TestCase):
-    def test_dummy(self):
-        assert True
+    def setUp(self):
+        self.yearinput = YearInput(name="yearinput")
+        self.graphinput = GraphingModeInput(name="graphinput")
+        self.season = SeasonInput(name="season")
+
+    def test_uni(self):
+    	self.assertEquals(unicode(self.yearinput), self.yearinput.name)
+    	self.assertEquals(unicode(self.graphinput), self.graphinput.name)
+    	self.assertEquals(unicode(self.season), self.season.name)
