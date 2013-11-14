0ce53f3a71ef1e23489daf823a4b17fb213b55f1
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2013-11-13 19:18:23 -0500

    ok engine is working

---
+++ wacep/weather_dj/engine.js
@@ -0,0 +1,132 @@
+importPackage(java.lang);
+
+
+var WeatherDJCore = (function() {
+	"use strict";
+    // settings:
+    var max_rain = 0.4;
+
+
+    // inputs
+    var a; // Fraction of rainwater that enters the ground per tick.
+    var b; // Fraction of rainfall that re-enters the atmosphere through evapo-transpiration per tick.
+    var c; // Fraction of groundwater that enters the stream channel per tick.
+    var r = 0.2; // rain probability per tick.
+
+
+    // outputs
+    var precipitation;
+    var runoff;
+    var groundwater;
+    var streamflow;
+    var errors; // should be null
+
+    // constructor
+    function WeatherDJCore(){
+    	groundwater = 1.0;
+    };
+
+	WeatherDJCore.prototype.getRain = function () {
+		"use strict";
+		var it_rained = (Math.random() < r);
+		if ( it_rained) {
+			return 	Math.random() * max_rain;
+		}
+		else {
+			return 0.0;
+		}
+	}
+
+
+    WeatherDJCore.prototype.tick = function() {
+		"use strict";
+		if (errors)  {
+			return;
+		}
+
+        precipitation = this.getRain();
+        var old_groundwater = groundwater;
+        runoff      = precipitation * (1-a-b);
+        streamflow  = runoff+(c *old_groundwater);
+        groundwater = (old_groundwater * (1-c))+(a*precipitation);
+    };
+
+
+    WeatherDJCore.prototype.validate = function() {
+        if (a + b > 1.0) {
+        	errors = 'a + b must be less than 1.0';
+        }
+        if (a > 1.0) {
+        	errors = 'c must be less than 1.0';
+        }
+        if (b > 1.0) {
+        	errors = 'c must be less than 1.0';
+        }
+        if (c > 1.0) {
+        	errors = 'c must be less than 1.0';
+        }
+        if (r > 1.0) {
+        	errors = 'r must be less than 1.0';
+        }
+
+    }
+
+    WeatherDJCore.prototype.setInputs = function(values) {
+		"use strict";
+        a = values ['a'];
+        b = values ['b'];
+        c = values ['c'];
+        r = values ['r'];
+		this.validate();
+        if (errors) {
+        	return;
+        }
+    };
+
+    WeatherDJCore.prototype.getOutputs= function() {
+		"use strict";
+        return {
+        	'precipitation': precipitation  ,
+        	'runoff'       : runoff         ,
+        	'groundwater'  : groundwater    ,
+        	'streamflow'   : streamflow     ,
+        	'errors'	   : errors         
+        };
+    };
+
+    return WeatherDJCore;
+})();
+
+
+////////////////
+//testing:
+
+
+
+function log(str) {
+	"use strict";
+	System.out.println(str);
+}
+
+
+
+function harness() {
+
+	"use strict";
+	//console.log ();
+
+	var weatherDJ = new WeatherDJCore();
+	weatherDJ.setInputs({'a':0.2,  'b': 0.1, 'c':0.3, 'r': 0.4});
+
+
+
+	for (var i=0;i<20;i++) { 
+			
+		weatherDJ.tick()
+		log (JSON.stringify(weatherDJ.getOutputs()));
+	}
+}
+
+
+
+harness();
