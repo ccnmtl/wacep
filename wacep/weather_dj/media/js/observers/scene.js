6e1e278067010fade1039b50f2d9e23a22a63317
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2013-11-22 14:41:10 -0500

    adding sky and river

--- wacep/weather_dj/media/js/observers/scene.js
+++ wacep/weather_dj/media/js/observers/scene.js
@@ -24,40 +24,10 @@ Scene.prototype.update = function ( ) {
 
 
     var wetness = info['groundwater'] * 50;
-    this.unsat_soil_ellipse.move(-600, - (500 + wetness))
+    this.unsat_soil_ellipse.animate().move(-600, - (500 + wetness))
 
-    var saturated_soil = this.unsat_soil_ellipse.maskWith(this.the_mask);
-    saturated_soil.fill ('#abc');
-
-
-
-
-    //ellipse1.maskWith(ellipse2)
-
-
-
-/*
-        var text = draw.text('SVG.JS').move(300, 300)
-        text.font({
-          family: 'Source Sans Pro'
-        , size: 180
-        , anchor: 'middle'
-        , leading: '1em'
-        })
-*/
-        // clip image with text
-  
-
-    //"M 100 100 L 300 100 L 200 300 z"
-    
-    //var path = draw.path('M 100 200 C 200 100 300 0 400 100 C 200 100 300 0 400 100 C 200 100 300 0 400 100 ').fill({ color: '#fff' })
-
-
-    //var polygon = draw.polygon('0,0 100,50 50,100').fill('none').stroke({ width: 1 })
-
-
-
-    //var polygon = draw.polygon('M 100 100 L 300 100 L 200 300 z').fill('none').stroke({ width: 1 })
+    var water_level = info['streamflow'] * 50;
+    this.river.animate().move (0,400 - water_level);
 
 /*
     update_div ('precipitation', info['precipitation'], jQuery('.scene_div.precipitation'));
@@ -118,18 +88,45 @@ Scene.prototype.prepareDOM = function () {
     "use strict";
 
     window.draw = SVG (jQuery('.scene')[0]);
+
+    this.sky = draw.rect(800, 600).move (0,-200).fill ('#2af');
+    this.river = draw.rect(800, 600).move (0,400).fill ('#26a');
+
+
+    //this.goat = draw.rect(800, 600).move (0,-200)
+
     this.saturated_soil_background = draw.image('/hydrologic_cycle/media/img/mask_1.jpg');
     this.saturated_soil_background.size(800, 800).y(-140);
+    this.big_rect = draw.rect(800, 600).move (0,-200).fill ('#654').maskWith (this.saturated_soil_background);
+
+
+
+
     this.the_mask = draw.image('/hydrologic_cycle/media/img/mask_1.jpg');
     this.the_mask.size(800, 800).y(-140);
 
 
     this.unsat_soil_ellipse =    draw.ellipse(2000,900);
+
     var initial_groundwater = 0; // should actually be initial value of groundwater.
     var wetness = initial_groundwater  * 50;
-    this.unsat_soil_ellipse.move(-600, - (500 + wetness))
-
+    this.unsat_soil_ellipse.move(-600, - (500 + wetness));
     var saturated_soil = this.unsat_soil_ellipse.maskWith(this.the_mask);
-    saturated_soil.fill ('#abc');
+    saturated_soil.fill ('#a98');
+
+    
+    
+    //this.blarg = draw.image('/hydrologic_cycle/media/img/mask_1.jpg').size(800, 800).y(-140).fill ('#fff');
+    
+    /*
+    this.mask = draw.rect(600, 100).move(0, 50).fill('#000');
+    
+    this.goat_1 = draw.rect(100, 600).move(50, 0).fill('#055');
+    this.goat   = draw.rect(100, 600).move(50, 0).fill('#000');
+    
+    thing_1 = this.goat.clipWith (this.mask);
+    thing_2 = this.goat_1.clipWith (thing_1)
+    */
+
     
 }
