cf747bbf668cd6d245836d311e1c90fc9cff686f
Eddie Rubeiz <eddie@ccnmtl.columbia.edu>
2013-11-22 10:51:39 -0500

    adding svg; initial experiments

--- wacep/weather_dj/media/js/observers/scene.js
+++ wacep/weather_dj/media/js/observers/scene.js
@@ -21,6 +21,41 @@ Scene.prototype.update = function ( ) {
     var c = jQuery('.slider.c').slider('value') / 100;
     var r = jQuery('.slider.r').slider('value') / 100;
 
+
+    //var ellipse1 =     draw.ellipse(300, 200).move(150, 100).fill({ color: '#fff' })
+    
+    //
+    var ellipse2 =    draw.ellipse(1000,500).move(-400,180).fill({ color: '#fff' })
+    //ellipse1.maskWith(ellipse2)
+
+    var image = draw.image('/hydrologic_cycle/media/img/mask_1.jpg')
+    image.size(800, 800).y(-140)
+
+/*
+        var text = draw.text('SVG.JS').move(300, 300)
+        text.font({
+          family: 'Source Sans Pro'
+        , size: 180
+        , anchor: 'middle'
+        , leading: '1em'
+        })
+*/
+        // clip image with text
+  
+    ellipse2.maskWith(image)
+
+    //"M 100 100 L 300 100 L 200 300 z"
+    
+    //var path = draw.path('M 100 200 C 200 100 300 0 400 100 C 200 100 300 0 400 100 C 200 100 300 0 400 100 ').fill({ color: '#fff' })
+
+
+    //var polygon = draw.polygon('0,0 100,50 50,100').fill('none').stroke({ width: 1 })
+
+
+
+    //var polygon = draw.polygon('M 100 100 L 300 100 L 200 300 z').fill('none').stroke({ width: 1 })
+
+/*
     update_div ('precipitation', info['precipitation'], jQuery('.scene_div.precipitation'));
     update_div ('groundwater',   info['groundwater'],   jQuery('.scene_div.groundwater'  ));
     update_div ('streamflow',    info['streamflow'],    jQuery('.scene_div.streamflow'   ));
@@ -33,7 +68,7 @@ Scene.prototype.update = function ( ) {
 
     update_div ('evapotranspiration_1',           b * info['precipitation'],          jQuery('.scene_div.evapotranspiration_1'       ));
     update_div ('evapotranspiration_2',           b * info['precipitation'],          jQuery('.scene_div.evapotranspiration_2'       ));
-
+*/
 
 
 }
@@ -63,14 +98,21 @@ function update_div(label, number, jq_obj) {
         jq_obj.height (number * 50);
     }
 
-
     if (label == 'evapotranspiration_1') {
         jq_obj.height (number * 50);
     }
 
-
     if (label == 'evapotranspiration_2') {
         jq_obj.height (number * 50);
     }
+
+}
+
+
+
+Table.prototype.prepareDOM = function () {
+    "use strict";
+
+    self.draw = SVG (jQuery('.scene')[0])
 
 }
