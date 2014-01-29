755ab12a114d8d10cc2256b1741f53582c5248fb
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2014-01-29 11:43:42 -0500

    test

--- wacep/timescale/tests/test_models.py
+++ wacep/timescale/tests/test_models.py
@@ -52,6 +52,17 @@ class TestTimescaleModel(TestCase):
             {'id': self.year.id,
              'name': self.year.name})
 
+
+    def test_input_combination_to_json(self):
+        self.to_json = self.inputcombination.to_json()
+        self.assertEquals(
+            self.to_json,
+            {'season_input_id': (self.inputcombination.season_input.id if self.inputcombination.season_input else None),
+             'graphing_mode_input_id': (self.inputcombination.graphing_mode_input.id if self.inputcombination.graphing_mode_input else None),
+             'year_input_id': (self.inputcombination.year_input.id if self.inputcombination.year_input else None),
+                 'activity_state_id': self.inputcombination.activity_state.id,
+                 'id': self.inputcombination.id})
+
     def test_activity_state_to_json(self):
         self.to_json = self.activitystate.to_json()
         self.assertEquals(
